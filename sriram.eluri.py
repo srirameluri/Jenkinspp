52d244cb8a9bcc577f82d7cdb7742219551707ff
srirameluri <34161268+srirameluri@users.noreply.github.com>
2026-09-18 11:56:23 -0400

    Create sriram.eluri.py

---
+++ sriram.eluri.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from Sriram Eluri Python Application!")
+    print("CI/CD Pipeline using Jenkins, Docker, GitHub, and Docker Hub")
+
+if __name__ == "__main__":
+    main()
